c91c50d4bdd65a36723c10a1e34173221258f19c
François BONIFACE <fraboniface@gmail.com>
2018-03-22 11:43:15 +0000

    resnet

---
+++ final/resGAN.py
@@ -0,0 +1,131 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable, grad
+import torch.nn.functional as F
+from torchvision  import transforms, datasets
+import torchvision.utils as vutils
+from tqdm import tqdm
+import sys
+import pickle
+
+from res_model import *
+
+model_name = 'ResSNWGAN'
+dataset_name = 'paintings64'
+SAVE_FOLDER = '../results/samples/{}/'.format(dataset_name)
+RESULTS_FOLDER = '../results/saved_data/'
+
+batch_size = 64
+zdim = 100
+n_feature_maps = 256
+n_epochs = 100
+
+print("Dataset creation...")
+transform = transforms.Compose(
+    [
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))
+    ])
+dataset = datasets.ImageFolder('../paintings64/', transform=transform)
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+print("Dataset created")
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv' or 'SNConv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+
+G = ResidualGenerator(zdim, n_feature_maps)
+D = ResidualCritic(n_feature_maps)
+G.apply(weights_init)
+D.apply(weights_init)
+
+fixed_z = torch.FloatTensor(batch_size, zdim, 1, 1).normal_(0,1)
+fixed_z = Variable(fixed_z, volatile=True)
+
+gpu = torch.cuda.is_available()
+if gpu:
+    G.cuda()
+    D.cuda()
+    fixed_z = fixed_z.cuda()
+
+lr = 1e-3
+beta1 = 0.5
+beta2 = 0.999
+G_optimiser = optim.Adam(G.parameters(), lr=lr, betas=(beta1, beta2))
+D_optimiser = optim.Adam(filter(lambda p: p.requires_grad, D.parameters()), lr=lr, betas=(beta1, beta2))
+
+train_hist = {
+    'D_loss': [],
+    'G_loss': []
+    }
+
+for epoch in tqdm(range(1,n_epochs+1)):
+    i = 0
+    D_losses = []
+    G_losses = []
+    for x, label in dataloader:
+        i += 1
+        print('{}: Epoch {}, batch {} starting...'.format(model_name, epoch, i))
+        if gpu:
+            x = x.cuda()
+
+        x = Variable(x)
+
+        # D training, n_critic=1
+        for p in D.parameters():
+            p.requires_grad = True
+
+        D.zero_grad()
+        D_real = D(x)
+
+        z = torch.FloatTensor(x.size(0), zdim, 1, 1).normal_()
+        if gpu:
+            z = z.cuda()
+
+        z = Variable(z)
+        fake = G(z)
+        D_fake = D(fake.detach())        
+        D_err = torch.mean(D_real) - torch.mean(D_fake)
+
+        D_err.backward()
+        D_optimiser.step()
+
+        # G training
+        for p in D.parameters():
+            p.requires_grad = False # saves computation
+
+        G.zero_grad()
+
+        z = torch.FloatTensor(batch_size, zdim, 1, 1).normal_()
+        if gpu:
+            z = z.cuda()
+
+        z = Variable(z)
+        fake = G(z)
+        G_err = torch.mean(D(fake))
+
+        G_err.backward()
+        G_optimiser.step()
+
+        D_losses.append(D_err.data[0])
+        G_losses.append(G_err.data[0])
+
+    # generates samples with fixed noise
+    fake = G(fixed_z)
+    vutils.save_image(fake.data, '{}{}_samples_epoch_{}.png'.format(SAVE_FOLDER, model_name, epoch), normalize=True, nrow=10)
+    train_hist['D_loss'].append(torch.mean(torch.FloatTensor(D_losses)))
+    train_hist['G_loss'].append(torch.mean(torch.FloatTensor(G_losses)))
+    with open(RESULTS_FOLDER + 'losses_{}_{}_epoch_{}.p'.format(dataset_name, model_name, epoch), 'wb') as f:
+        pickle.dump(train_hist, f)
+
+    if epoch % 5 == 0:
+        # saves everything, overwriting previous epochs
+        torch.save(G.state_dict(), RESULTS_FOLDER + '{}_{}_epoch_{}_generator'.format(dataset_name, model_name, epoch))
+        torch.save(DQ.state_dict(), RESULTS_FOLDER + '{}_{}_epoch_{}_D_and_Q'.format(dataset_name, model_name, epoch))
---
+++ final/res_model.py
@@ -0,0 +1,128 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.modules import conv
+from torch.nn.modules.utils import _pair
+  
+
+class MinibatchSDLayer(nn.Module):
+    def __init__(self):
+        super(MinibatchSDLayer, self).__init__()
+        
+    def forward(self, x):
+        mean_batch_std = x.std(0).mean()
+        mean_batch_std = mean_batch_std.expand(x.size(0), 1, x.size(-1), x.size(-1))
+        return torch.cat([x, mean_batch_std], 1)
+
+class PixelWiseFeatureNormLayer(nn.Module):
+    def __init__(self):
+        super(PixelWiseFeatureNormLayer, self).__init__()
+        
+    def forward(self, x):
+        return x / x.norm(2,1)
+
+
+def l2normalize(v, eps=1e-12):
+    return v / (v.norm() + eps)
+
+def max_singular_value(W, u=None, Ip=1):
+    """
+    power iteration for weight parameter
+    """
+    if u is None:
+        u = torch.FloatTensor(1, W.size(0)).normal_()
+        
+    _u = u
+    for _ in range(Ip):
+        _v = l2normalize(torch.matmul(_u, W), eps=1e-12)
+        _u = l2normalize(torch.matmul(_v, torch.transpose(W, 0, 1)), eps=1e-12)
+        
+    sigma = torch.sum(F.linear(_u, torch.transpose(W, 0, 1)) * _v)
+    return sigma, _u
+    
+    
+class SNConv2d(conv._ConvNd):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True):
+        kernel_size = _pair(kernel_size)
+        stride = _pair(stride)
+        padding = _pair(padding)
+        dilation = _pair(dilation)
+        super(SNConv2d, self).__init__(in_channels, out_channels, kernel_size, stride, padding, dilation, False, _pair(0), groups, bias)
+        self.u = nn.Parameter(torch.Tensor(1, out_channels).normal_(), requires_grad=False)
+        #self.u = torch.Tensor(1, out_channels).normal_()
+
+    @property
+    def W_(self):
+        w_mat = self.weight.view(self.weight.size(0), -1).data
+        sigma, _u = max_singular_value(w_mat, self.u.data)
+        self.u.data = _u
+        return self.weight / sigma
+
+    def forward(self, input):
+        return F.conv2d(input, self.W_, self.bias, self.stride, self.padding, self.dilation, self.groups)
+
+
+def block(in_fm, out_fm):
+    l = [
+        nn.Conv2d(in_fm, out_fm, 3, 1, 1, bias=True),
+        nn.LeakyReLU(0.2, inplace=True),
+        nn.Conv2d(out_fm, out_fm, 3, 1, 1, bias=True),
+        nn.LeakyReLU(0.2, inplace=True)
+    ]
+    return nn.Sequential(*l)
+        
+
+class ResidualGenerator(nn.Module):
+    def __init__(self, zdim=100, n_feature_maps=256):
+        super(ResidualGenerator, self).__init__()
+
+        self.from_latent = nn.ConvTranspose2d(zdim, 2*n_feature_maps, 4, 1, 0, bias=True)
+        self.block1 = block(2*n_feature_maps, 2*n_feature_maps)
+        self.upsample = nn.Upsample(scale_factor=2, mode='nearest')
+        self.block2 = block(2*n_feature_maps, n_feature_maps)
+        self.skip = nn.Conv2d(2*n_feature_maps,n_feature_maps, 1, 1, 0, bias=True)
+        self.block3 = block(n_feature_maps, n_feature_maps)
+        self.to_rgb = block(n_feature_maps, 3)
+                
+    def forward(self, x):
+        x = self.from_latent(x) # 4x4x2nc
+        x = self.upsample(x)
+        x = self.block1(x) + x # 8x8x2nc
+        x = self.upsample(x)
+        x = self.block2(x) + self.skip(x) #16x16xnc
+        x = self.upsample(x)
+        x = self.block3(x) + x #32x32xnc
+        x = self.upsample(x)
+        x = self.to_rgb(x) #64x64x3
+        return F.tanh(x)
+
+
+class ResidualCritic(nn.Module):
+    def __init__(self, zdim=100, n_feature_maps=256):
+        super(ResidualCritic, self).__init__()
+
+        self.from_rgb = block(3, n_feature_maps)
+        self.downsample = nn.AvgPool2d(2)
+        self.block1 = block(n_feature_maps, n_feature_maps)
+        self.block2 = block(n_feature_maps, 2*n_feature_maps)
+        self.skip2 = SNConv2d(n_feature_maps,2*n_feature_maps, 1, 1, 0, bias=True)
+        self.block3 = block(2*n_feature_maps, 2*n_feature_maps)
+        self.block4= block(2*n_feature_maps, 2*n_feature_maps)
+        self.mbstd = MinibatchSDLayer()
+        self.last_conv = SNConv2d(2*n_feature_maps+1, 4*n_feature_maps, 4, 1, 0, bias=True)
+        self.fc = SNConv2d(4*n_feature_maps, 1, 1, 1, 0, bias=True)
+
+    def forward(self, x):
+        x = self.from_rgb(x) #64x64xnc
+        x = self.downsample(x)
+        x = self.block1(x) + x #32x32xnc
+        x = self.downsample(x)
+        x = self.block2(x) + self.skip2(x) #16x16x2nc
+        x = self.downsample(x)
+        x = self.block3(x) + x #8x8x2nc
+        x = self.downsample(x)
+        x = self.block4(x) + x #4x4xnc
+        x = self.mbstd(x)
+        x = self.last_conv(x) #1x1x4nc
+        x = self.fc(x) #1x1x1
+        return x.view(-1,1).squeeze(1)
